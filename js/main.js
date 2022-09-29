d4dd6ec9fe9e7bcb0f2427d4cf9f904e37b46131
Nitish Jha <57320225+Aiden16@users.noreply.github.com>
2022-09-29 13:10:34 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,32 @@
 const about = document.querySelector('#about')
 const contact = document.querySelector('#contact')
+const project = document.querySelector('#projects')
 const aboutContent = document.querySelector('#about-content')
 const contactContent = document.querySelector('#contact-content')
+const projectsContent = document.querySelector('#projects-content')
+
+project.addEventListener('click', () => {
+  const aboutBox = new WinBox({
+      title:'My Projects',
+      width:'400px',
+      height:'400px',
+      top:50,
+      left:50,
+      right:50,
+      bottom:50,
+      mount:projectsContent,
+      onfocus: function () {
+        this.setBackground('#00aa00')
+      },
+      onblur: function () {
+        this.setBackground('#777')
+      },
+  })
+})
 
 about.addEventListener('click', () => {
   const aboutBox = new WinBox({
       title:'About Me',
-    //   background:'#00aa00',
-    // modal:true,
       width:'400px',
       height:'400px',
       top:50,
